3aab9aaab5fafb417e731c2b8ef3e00150cfa1b5
yan <yan.braverman37@gmail.com>
2024-01-27 12:50:30 +0200

    Add types

---
+++ src/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/* let abc = 'str';
+
+let a = 30;
+
+const b = 20;
+
+// Depricated
+var c = 50; */
+
+/* console.log(a + b);
+console.log(a); */
+
+let getFirstName = function () {};
+
+const MATH_PI = Math.PI;
+
+const COLOR_GREEN = '#0F0';
+
+const user = {
+	name: 'Jhon',
+	age: 20,
+	isMarried: true,
+	address: {
+		street: 'jujj',
+		city: 'ddf',
+	},
+	0: '0',
+	true: false,
+};
+
+// console.log(2 ** 53 - 1);
+
+let num = 20;
+let bigNum = 20n;
+
+let bull = true;
+
+bull = 20;
+
+let arr = [1];
+
+let str = '';
+
+console.log(typeof bigNum);
+console.log(typeof num);
+console.log(typeof null);
+console.log(2 / 0);
