addfa2343b31908504759e763ce1bdbb894e3e43
yan <yan.braverman37@gmail.com>
2024-01-28 14:02:27 +0200

    Add loops

--- src/op-loops.js
+++ src/op-loops.js
@@ -23,10 +23,21 @@ while(count < 10){
 
 // Multiply table
 
-outer: for (let i = 1; i < 10; i++) {
+/* outer: for (let i = 1; i < 10; i++) {
 	inner: for (let j = 1; j < 10; j++) {
 		if (i === 5) continue outer;
 		if (j === 8) continue outer;
 		console.log(`${i} x ${j} = ${i * j}`);
 	}
+} */
+
+//  Task summ until 100
+
+let result = 0;
+let num = 90;
+
+for (; num <= 100; num++) {
+	result += num;
 }
+console.log(result);
+let str = 'str';
