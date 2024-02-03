94e975d6baded0039ca77d88dfa8e46483200786
yan <yan.braverman37@gmail.com>
2024-02-03 11:58:35 +0200

    Add change arg

--- src/functions.js
+++ src/functions.js
@@ -34,29 +34,53 @@ console.log(typeof newFunc)
 console.log(newFunc(20, 40)) */
 
 // Check age
-let age = 20;
+/* let age = 20;
 // let welcome;
 if (age < 18) {
 	function welcome() {
 		console.log('You are too much young');
 	}
-	/* welcome = function() {
-		console.log('You are too much young');
-	}; */
+	// welcome = function() {
+	// 	console.log('You are too much young');
+	// };
 	welcome();
 } else {
 	function welcome() {
 		console.log('You are enough adult');
 	}
-	/* welcome = function () {
-		console.log('You are enough adult');
-	}; */
+	// welcome = function () {
+	// 	console.log('You are enough adult');
+	// };
 	welcome();
 }
-// welcome();
+// welcome(); */
 
 // Arrow functions
 
-const function3 = (par) => par * 2;
+const function3 = (par1, par2) => {
+    return par1 ** par2;
+};
 
-console.log(function3(5));
+console.log(function3(8, 2));
+
+//  Change global var as param
+/* let brand = 'Stiga';
+
+let ttBlade = 'Donic';
+
+function changeArg(brand = 'TSP', ttBlade = 'Xiom') {
+	// brand = 'Butterfly';
+	// ttBlade = 'DHS';
+	console.log(brand);
+	console.log(ttBlade);
+	return `${brand} + ${ttBlade}`;
+}
+console.log(changeArg());
+console.log(brand);
+console.log(ttBlade); */
+
+function add(a, b) {
+    return (a ?? 10) + (b ?? 20);
+}
+
+console.log(add())
