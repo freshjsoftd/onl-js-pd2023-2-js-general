c0a024757cc69f5bd8a57f986057bd9c74163c35
yan <yan.braverman37@gmail.com>
2024-02-03 11:10:08 +0200

    Add arrow function

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,6 @@
 'use strict';
 // Function Declaration
-console.log(summ(40, 50));
+/* console.log(summ(40, 50));
 function summ(a, b) {
 	// console.log(a + b + c);
 
@@ -31,4 +31,32 @@ const newFunc = showDivide();
 
 console.log(typeof newFunc)
 
-console.log(newFunc(20, 40))
+console.log(newFunc(20, 40)) */
+
+// Check age
+let age = 20;
+// let welcome;
+if (age < 18) {
+	function welcome() {
+		console.log('You are too much young');
+	}
+	/* welcome = function() {
+		console.log('You are too much young');
+	}; */
+	welcome();
+} else {
+	function welcome() {
+		console.log('You are enough adult');
+	}
+	/* welcome = function () {
+		console.log('You are enough adult');
+	}; */
+	welcome();
+}
+// welcome();
+
+// Arrow functions
+
+const function3 = (par) => par * 2;
+
+console.log(function3(5));
