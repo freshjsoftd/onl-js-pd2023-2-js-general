760f67875f50b1a66b4580a12a25ef6aba2e6930
yan <yan.braverman37@gmail.com>
2024-02-18 11:35:30 +0200

    Add rest and spread

--- src/prototype.js
+++ src/prototype.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Country(title = 'Ukraine', capital = 'Kiev') {
+/* function Country(title = 'Ukraine', capital = 'Kiev') {
 	this.title = title;
 	this.capital = capital;
 	// this.getInfo = function(){
@@ -21,4 +21,26 @@ console.log(usa.getInfo());
 console.log(ukraine.getInfo === usa.getInfo);
 console.log(ukraine);
 
-console.log(usa instanceof Array)
+console.log(usa instanceof Array) */
+
+// arguments-spread-rest
+function someFunc(a) {
+	return arguments;
+}
+
+const funcExp = function (a) {
+    // const arr = Array.from(arguments);
+    // arr.forEach(el => console.log(el))
+	return arguments;
+};
+
+const arrowFunc = (a, ...args) => args;
+
+console.dir(someFunc);
+console.dir(funcExp);
+console.dir(arrowFunc);
+
+console.log(someFunc(1, 5, true, {}));
+console.log(funcExp(1, 5, true, {}));
+const arrArgs = arrowFunc(1, 5, true, {})
+console.log(arrArgs);
