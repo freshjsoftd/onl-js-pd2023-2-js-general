fcbb9ee7a91870ac26f3813701e7575d733b5203
yan <yan.braverman37@gmail.com>
2024-02-18 14:13:37 +0200

    Add map, reverse, concat and destructure

--- src/prototype.js
+++ src/prototype.js
@@ -1,46 +1,143 @@
 'use strict';
 
-/* function Country(title = 'Ukraine', capital = 'Kiev') {
-	this.title = title;
-	this.capital = capital;
-	// this.getInfo = function(){
-	//     return `Capital is ${this.capital}`
-	// }
+function MyArray(...args) {
+	this.length = 0;
+	// this.push = function (item) {
+	// 	this[this.length] = item;
+	// 	return ++this.length;
+	// };
+	for (let i = 0; i < args.length; i++) {
+		this.push(args[i]);
+	}
 }
 
-Country.prototype.getInfo = function () {
-	return `Capital is ${this.capital}`;
+MyArray.isMyArray = function (obj) {
+	return obj instanceof MyArray;
 };
 
-const ukraine = new Country();
-const usa = new Country('USA', 'Washington');
+MyArray.prototype = new MyArrayProto();
 
-console.log(ukraine.getInfo());
-console.log(usa.getInfo());
+const myArr = new MyArray(10);
+const myArr1 = new MyArray(100, 500, 700, 900, 1000);
 
-console.log(ukraine.getInfo === usa.getInfo);
-console.log(ukraine);
+const arr = [20, 30];
+const arr1 = [];
 
-console.log(usa instanceof Array) */
+console.log(arr.push === arr1.push);
+console.log(myArr.push === myArr1.push);
 
-// arguments-spread-rest
-function someFunc(a) {
-	return arguments;
+console.log(myArr);
+console.log(myArr1.pop());
+console.log(MyArray.isMyArray(arr));
+
+console.dir(MyArray);
+
+function MyArrayProto() {
+	this.push = function () {
+		if (arguments) {
+			for (let i = 0; i < arguments.length; i++) {
+				this[this.length++] = arguments[i];
+			}
+		}
+		return this.length;
+	};
+	this.pop = function () {
+		if (this.length === 0) {
+			return;
+		}
+		const lastItem = this[this.length - 1];
+		delete this[--this.length];
+		return lastItem;
+	};
+
+	this.forEach = function (fn) {
+		for (let i = 0; i < this.length; i++) {
+			fn(this[i], i, this);
+		}
+	};
+
+	this.some = function (fn) {
+		for (let i = 0; i < this.length; i++) {
+			if (fn(this[i], i, this)) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	this.every = function (fn) {
+		for (let i = 0; i < this.length; i++) {
+			if (!fn(this[i], i, this)) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	// map
+	this.map = function (fn) {
+		const res = new MyArray();
+		for (let i = 0; i < this.length; i++) {
+			res.push(fn(this[i], i, this));
+		}
+		return res;
+	};
+
+	// concat
+	this.concat = function (...args) {
+		const res = new MyArray();
+		for (let i = 0; i < args.length; i++) {
+			if (Array.isArray(args[i])) {
+				res.push(...args[i]);
+			} else if (MyArray.isMyArray(args[i])) {
+				for (let j = 0; j < args[i].length; j++) {
+					res.push(args[i][j]);
+				}
+			} else {
+				res.push(args[i]);
+			}
+		}
+		return res;
+	};
+
+	// reverse
+	this.reverse = function () {
+		const lengthMinusOne = this.length - 1;
+        // let temp;
+		for (let i = 0; i < Math.floor(this.length / 2); i++) {
+			// temp = this[i];
+			// this[i] = this[lengthMinusOne - i];
+			// this[lengthMinusOne - i] = temp;
+            [this[i], this[lengthMinusOne - i]] = 
+            [this[lengthMinusOne - i], this[i]]
+		}
+        return this;
+	};
 }
 
-const funcExp = function (a) {
-    // const arr = Array.from(arguments);
-    // arr.forEach(el => console.log(el))
-	return arguments;
-};
+console.log(myArr1)
+console.log(myArr1.reverse())
 
-const arrowFunc = (a, ...args) => args;
+// const myArrMix = new MyArray(1);
 
-console.dir(someFunc);
-console.dir(funcExp);
-console.dir(arrowFunc);
+console.log(myArr.concat([1, 3], true, myArr1));
 
-console.log(someFunc(1, 5, true, {}));
-console.log(funcExp(1, 5, true, {}));
-const arrArgs = arrowFunc(1, 5, true, {})
-console.log(arrArgs);
+// destucture
+let a = 10;
+let b = 100;
+
+[a, b] = [b, a];
+
+console.log(a)
+console.log(b)
+
+
+
+const person = ['Jhon', 'Doe'];
+
+let [fName, lName] = person;
+
+// let fName = person[0];
+// let lName = person[1];
+console.log(fName)
+console.log(lName);
