5989d49c0fcb0f53a640b7a96ca54ec9dbd1b2c0
yan <yan.braverman37@gmail.com>
2024-03-16 13:20:30 +0200

    Add Promise API

--- src/promise.js
+++ src/promise.js
@@ -15,10 +15,10 @@ import { newRateSpan } from './events.js';
     }, 2000) */
 // Promise
 const promise = new Promise((resolve, reject) => {
-	setTimeout(() => resolve('Is done'), 2000);
 	setTimeout(() => reject(new Error('Smth went wrong')), 2000);
-})
-	.then((value) => {
+	setTimeout(() => resolve('Is done'), 2000);
+});
+/* .then((value) => {
 		console.log(value);
 		return value + ' it all';
 	})
@@ -35,17 +35,18 @@ const promise = new Promise((resolve, reject) => {
     // .then(null, (error) => {
     //     console.log(error.message);
     //     newRateSpan.textContent = error.message;
-    // })
+    // }) 
     .catch((error) => {
         console.log(error.message);
         newRateSpan.textContent = error.message;
+        return error.message;
     })
-
+*/
 // promise.then((value) => value + 'Smth')
 
 console.log(promise);
 // Function return promise
-function sum(a, b){
+/* function sum(a, b){
     return new Promise((resolve) => {
         const result = a + b;
         resolve(result);
@@ -54,6 +55,43 @@ function sum(a, b){
 
 sum(5, 20)
     .then((value) => `${value}$`)
-    .then(summ => console.log(summ))
+    .then(summ => console.log(summ)) */
 // Promise API
 // method all()
+const p1 = Promise.resolve(3);
+let p2 = 137;
+const p3 = new Promise((resolve, reject) => {
+	setTimeout(() => reject('Stiga'), 1000);
+	setTimeout(() => resolve('Stiga'), 1000);
+	// setTimeout(resolve, 1000, 'Stiga');
+});
+const arrPromise = [promise, p3];
+
+/* Promise.all(arrPromise)
+    .then(values => {
+        console.log(values)
+    })
+    .catch((error) => {
+        console.log(error);
+    }) */
+// method allSettled()
+/*Promise.allSettled(arrPromise)
+    .then((values) => {
+	console.log(values);
+        }) */
+// method race()
+/* Promise.race(arrPromise)
+.then(values => {
+    console.log(values)
+})
+.catch((error) => {
+    console.log(error.message);
+}) */
+// method any()
+Promise.any(arrPromise)
+	.then((values) => {
+		console.log(values);
+	})
+	.catch((error) => {
+		console.log(error.errors);
+	});
